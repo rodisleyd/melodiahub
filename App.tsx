e0d69293dcb2a72d2b4361906b51732171b410f2
MelodiaHub Deploy <deploy@melodiahub.com>
2026-02-04 20:31:39 -0300

    Fix duplicate return statement in App.tsx

--- App.tsx
+++ App.tsx
@@ -347,7 +347,7 @@ const AppContent: React.FC = () => {
                   }
 
                   return (
-                  return (
+
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-3">
                       {topTracks.map((track, idx) => (
                         <div
